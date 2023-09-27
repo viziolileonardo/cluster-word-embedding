f67a02e764d486af71427781fd5e92d8c0bf05fe
Leonardo Vizioli <viziolileonardo@gmail.com>
2023-09-27 11:09:36 +0100

    general questions

--- embeddings.py
+++ embeddings.py
@@ -6,11 +6,11 @@ import pandas as pd
 embed = hub.load("https://tfhub.dev/google/universal-sentence-encoder/4")
 
 # Load your CSV file into a DataFrame
-csv_path = "YOURCSV.csv"
+csv_path = "/Users/vizioli/git/teya/cluster-word-embedding/Caselist_1.csv"
 data = pd.read_csv(csv_path)
 
 # Get the text from the "Internal CR Agents Notes" column
-notes = data["YOURCOLUMNNAME"].tolist()
+notes = data["Internal Notes (Case)"].tolist()
 
 # Generate embeddings for the notes
 note_embeddings = embed(notes)
@@ -22,5 +22,5 @@ embedding_df = pd.DataFrame(note_embeddings.numpy(), columns=[f"embedding_{i}" f
 result_df = pd.concat([data, embedding_df], axis=1)
 
 # Save the updated DataFrame to a new CSV file
-output_csv_path = "YOUROUTPUTFILE.csv"
+output_csv_path = "generalquestionswithembeddings.csv"
 result_df.to_csv(output_csv_path, index=False)
